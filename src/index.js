d615dea13407ac47e56517a09fd4fe9228200edf
Mikael Svensson <113359307+mikeCre8@users.noreply.github.com>
2023-03-29 09:08:45 +0200

    init

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import "bootstrap/dist/css/bootstrap.css";
 
 const greeting = <h1>Welcome</h1>;
 
-ReactDOM.createRoot(document.getElementById("root")).render(greeting) // all required node_modules are installed. Ready to commit & push. (Step 11)
+ReactDOM.createRoot(document.getElementById("root")).render(greeting) 
